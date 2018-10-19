9dc18a04fa9cdc85999eb06cd695a368f26431f6
Luis Solorzano <luis.solo@est.unanleon.edu.ni>
2018-10-18 19:53:28 -0600

    Validate form field

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
+'use strict';
+
 (() => {
-	alert('Luis');
+	document.getElementById('btn-send').addEventListener('click', (event) => {
+		document.querySelectorAll('input, select').forEach((value, key, parent) => {
+			if (value.value !== '') {
+				alert("Todos los datos son correctos")
+			} else {
+				alert('El campo "'+ value.id+'" es requerido.');
+			}
+		});
+	});
 })();
+
