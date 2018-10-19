5f0bd7361af546d82f27797b69fb07d1d47566cb
Luis Solorzano <luis.solo@est.unanleon.edu.ni>
2018-10-18 20:29:59 -0600

    Reformat Code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,15 @@
 'use strict';
 
 (() => {
-	document.getElementById('btn-send').addEventListener('click', (event) => {
-		document.querySelectorAll('input.input-form, select.input-form').forEach((value, key, parent) => {
-			if (value.value !== '') {
-				alert('El campo "'+ value.id+'" es valido.');
-			} else {
-				console.log(value);
-				alert('El campo "'+ value.id+'" es requerido.');
-			}
-		});
-	});
+  document.getElementById('btn-send').addEventListener('click', (event) => {
+    document.querySelectorAll('input.input-form, select.input-form').forEach((value, key, parent) => {
+      if (value.value !== '') {
+        alert('El campo "' + value.id + '" es valido.');
+      } else {
+        console.log(value);
+        alert('El campo "' + value.id + '" es requerido.');
+      }
+    });
+  });
 })();
 
